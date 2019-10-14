4d83fee0dd296b4bed92b48d49e60ad4e9580489
markpriston13133 <56536835+markpriston13133@users.noreply.github.com>
2019-10-14 04:48:53 -0400

    Create config-sample.py

---
+++ config-sample.py
@@ -0,0 +1,10 @@
+# config file. put here your parse.com keys
+def config():
+   C = {
+     'APPLICATION_ID': 'put your key here',
+     'REST_API_KEY' : 'and here',
+     'MASTER_KEY' : "and here"
+   }
+   return C
+
+# dont forget to rename this file to config.py
